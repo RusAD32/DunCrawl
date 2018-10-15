0c8be127496be667ff1569a7e28fbdda133242ba
Vseslav Kochenov <kvv@tcinet.ru>
2018-10-16 00:08:34 +0300

    Small fix

--- Interfaces/Chest.go
+++ Interfaces/Chest.go
@@ -1,33 +1,31 @@
 package Interfaces
 
 type Chest struct {
-    Trap
-    Loot []Lootable
-    UsefulLoot []Carriable
+	Trap       Trap
+	Loot       []Lootable
+	UsefulLoot []Carriable
 }
 
 func (c *Chest) TrapTrigger(p *Player) {
-    if c.Trap != nil {
-	c.Trap.Trigger(p)
-    }
+	if c.Trap != nil {
+		c.Trap.Trigger(p)
+	}
 }
 
 func (c *Chest) TrapDisarm() {
-    if c.Trap != nil {
-        c.Trap = nil
-    }
+	if c.Trap != nil {
+		c.Trap = nil
+	}
 }
 
 func (c *Chest) GetMoney() int {
-    total := 0
-    for _, v := range c.Loot {
-        total += v.GetValue()
-    }
-    return total
+	total := 0
+	for _, v := range c.Loot {
+		total += v.GetValue()
+	}
+	return total
 }
 
 func (c *Chest) GetValuables() []Carriable {
-    return c.UsefulLoot
+	return c.UsefulLoot
 }
-
-
