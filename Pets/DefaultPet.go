bbfb079ae798300457c341d8210b07e70a93b238
Vseslav Kochenov <kvv@tcinet.ru>
2019-01-07 02:44:25 +0300

    Forgot to add file to the prevous commit

---
+++ Pets/DefaultPet.go
@@ -0,0 +1,23 @@
+package Pets
+
+import (
+	. "DunCrawl/EnemySkills"
+	. "DunCrawl/Interfaces"
+)
+
+type DefaultPet Pet
+
+func (d *DefaultPet) Init() *Pet {
+	skills := make([]NPCSkill, 0)
+	sk1 := new(DogBite).Init(d)
+	skills = append(skills, sk1)
+	e := new(Pet).Initialize(
+		Animal,
+		skills,
+		Usual,
+		"Doggy",
+		25,
+		make(map[Stat]int))
+	d = (*DefaultPet)(e)
+	return e
+}
